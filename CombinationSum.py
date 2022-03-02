51a29fd2e8ac748390be80a7c9978b2ba232c3f2
Nitink2001 <66001925+Nitink2001@users.noreply.github.com>
2022-03-02 18:57:28 +0530

    Create CombinationSum.py

---
+++ CombinationSum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        
+        res=[]
+        ans=[]
+        def backtrack(i,target):
+            
+            if target<0:return
+            if target==0:
+                res.append(ans[:])
+                return
+            
+            for j in range(i,len(candidates)):
+
+                ans.append(candidates[j])
+                backtrack(j,target-candidates[j])
+                ans.pop()
+                
+        backtrack(0,target)
+        return res
+                
+                
